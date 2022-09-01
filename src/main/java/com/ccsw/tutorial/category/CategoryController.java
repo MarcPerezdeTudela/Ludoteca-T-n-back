381b452d3c0427d086667d6a87de125f4bf53673
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-09-01 12:07:40 +0200

    end 'Code: Conectar front con back' section

--- src/main/java/com/ccsw/tutorial/category/CategoryController.java
+++ src/main/java/com/ccsw/tutorial/category/CategoryController.java
@@ -3,6 +3,7 @@ package com.ccsw.tutorial.category;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.devonfw.module.beanmapping.common.api.BeanMapper;
  */
 @RequestMapping(value = "/category")
 @RestController
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     @Autowired
